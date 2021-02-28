7bd0317a98edc46637e2ba90739da54aadf2a0d9
mechanicworld <47930174+mechanicworld@users.noreply.github.com>
2021-02-28 16:02:05 +0300

    Components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router, Route} from "react-router-dom"
+
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercises.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
 
 function App() {
   return (
-    <div className="container">
-      Hello world
-    </div>
+    <Router>
+      <Navbar/>
+      <br/>
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
+    
   );
 }
 
